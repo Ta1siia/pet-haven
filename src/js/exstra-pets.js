c0f8e33a86c87832311bccd6cb0cfebf7bd3ff90
taisiia <twitlie@proton.me>
2026-06-17 16:06:44 +0300

    remove variables

--- src/js/exstra-pets.js
+++ src/js/exstra-pets.js
@@ -1,8 +1,6 @@
-
 const DESKTOP_WIDTH = 1440;
 const MOBILE_TABLET_LIMIT = 8;
 const DESKTOP_LIMIT = 9;
-
 
 export function getItemsPerPage() {
   return window.innerWidth >= DESKTOP_WIDTH
@@ -10,18 +8,9 @@ export function getItemsPerPage() {
     : MOBILE_TABLET_LIMIT;
 }
 
-export let perPage = getItemsPerPage();
-export let totalPages = 1;
-
-
 export function updateActiveCategory(activeButton) {
   const buttons = document.querySelectorAll('.category-btn');
 
   buttons.forEach(button => button.classList.remove('is-active'));
   activeButton.classList.add('is-active');
 }
-
-
-
-
-
